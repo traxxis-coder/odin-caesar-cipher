67d4cff11c49f031549a34ced9996ff6b0cc4d96
traxxis-coder <cigatomas@gmail.com>
2025-12-19 13:13:18 +0100

    Add a function for decoding - cycles through all shift values at once to find all the possible phrases - also found a bug that would result in index of 0 - FIXED

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,61 @@
 def caesar_cipher(string, shift)
-  letters = {
+  word_array = string.split
+  result = word_array.map do |word|
+    word.split("").map do |letter|
+      if $letters.values.include?(letter.downcase)
+        up_case = letter.upcase == letter
+        index = $letters.key(letter.downcase)
+        up_case ? $letters[(index + shift) % 26 == 0 ? 26 : (index + shift) % 26].upcase : $letters[(index + shift) % 26 == 0 ? 26 : (index + shift) % 26]
+      else
+        letter
+      end
+    end.join("")
+  end.join(" ")
+  puts result
+end
+
+def caesar_cipher_decode(string)
+  word_array = string.split
+  25.times do |num|
+    shift = num + 1
+    result = word_array.map do |word|
+      word.split("").map do |letter|
+        if $letters.values.include?(letter.downcase)
+          up_case = letter.upcase == letter
+          index = $letters.key(letter.downcase)
+          up_case ? $letters[(index + shift) % 26 == 0 ? 26 : (index + shift) % 26].upcase : $letters[(index + shift) % 26 == 0 ? 26 : (index + shift) % 26]
+        else
+          letter
+        end
+      end.join("")
+    end.join(" ")
+    puts result
+  end
+end
+
+def get_shift
+  puts "Enter the shift value for the cipher:"
+  shift = gets.chomp.to_i
+  if shift > 26
+    shift = shift % 26
+  elsif shift < 1
+    shift = (26 + shift) % 26
+  end
+  shift
+end
+
+def get_string
+  puts "Enter the string you want to use Caesar's cipher on:"
+  string = gets.chomp
+  string
+end
+
+def decode?
+  puts "Type 'decode' to decode a phrase coded with the Caesar's cipher. Otherwise you can code a phrase with a custom shift value."
+  gets.chomp.downcase == "decode"
+end
+
+$letters = {
   1 => "a",
   2 => "b",
   3 => "c",
@@ -26,30 +82,6 @@ def caesar_cipher(string, shift)
   24 => "x",
   25 => "y",
   26 => "z"
-  }
-  if shift > 26
-    shift = shift % 26
-  elsif shift < 1
-    shift = (26 + shift) % 26
-  end
-  word_array = string.split
-  word_array.map! do |word|
-    word.split("").map do |letter|
-      if letters.values.include?(letter.downcase)
-        up_case = letter.upcase == letter
-        index = letters.key(letter.downcase)
-        up_case ? letters[(index + shift) % 26].upcase : letters[(index + shift) % 26]
-      else
-        letter
-      end
-    end.join("")
-  end.join(" ")
-end
+}
 
-puts "Enter the string you want to use Caesar's cipher on:"
-string = gets.chomp
-
-puts "Enter the shift value for the cipher:"
-shift = gets.chomp.to_i
-
-puts caesar_cipher(string, shift)
+decode? ? caesar_cipher_decode(get_string) : caesar_cipher(get_string, get_shift)
